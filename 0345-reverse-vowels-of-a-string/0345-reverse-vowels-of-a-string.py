0bc68207ddd62730503f736303ad0ce59da6abe0
Nilay Jain <31623181+nilayjain12@users.noreply.github.com>
2025-01-11 00:09:56 -0800

    Time: 26 ms (17.01%), Space: 19.6 MB (9.45%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -5,20 +5,20 @@ class Solution:
         '''
         s_list = [ch for ch in s]
         print(s_list)
-        vowels = {'a':1, 'e':1, 'i':1, 'o':1, 'u':1, 'A':1, 'E':1, 'I':1, 'O':1, 'U':1}
+        vowels = {'a':1, 'e':1, 'i':1, 'o':1, 'u':1}
         i, j = 0, len(s)-1
 
         while i < j:
             # check if both i, j are vowels
-            if s_list[i] in vowels and s_list[j] in vowels:
+            if s_list[i].lower() in vowels and s_list[j].lower() in vowels:
                 s_list[i], s_list[j] = s_list[j], s_list[i]
                 i += 1
                 j -= 1
             # check if j is not vowel
-            elif s_list[i] in vowels and s_list[j] not in vowels:
+            elif s_list[i].lower() in vowels and s_list[j].lower() not in vowels:
                 j -= 1
             # check if i is not vowel
-            elif s_list[j] in vowels and s_list[i] not in vowels:
+            elif s_list[j].lower() in vowels and s_list[i].lower() not in vowels:
                 i += 1
             else:
                 i += 1
