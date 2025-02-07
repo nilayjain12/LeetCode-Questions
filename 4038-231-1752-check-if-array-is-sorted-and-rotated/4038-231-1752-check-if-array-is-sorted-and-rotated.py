1275dd6c76b3ea6cc566ae46915c12cdfe8b7ed6
Nilay Jain <31623181+nilayjain12@users.noreply.github.com>
2025-02-07 12:43:47 -0800

    Time: 0 ms, Memory: 17.5 MB - LeetHub

--- 4038-231-1752-check-if-array-is-sorted-and-rotated/4038-231-1752-check-if-array-is-sorted-and-rotated.py
+++ 4038-231-1752-check-if-array-is-sorted-and-rotated/4038-231-1752-check-if-array-is-sorted-and-rotated.py
@@ -1,29 +1,25 @@
 from collections import deque
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        sorted_nums = nums.copy()
-        sorted_nums.sort()
-        # find all positions of the first element of nums in the sorted list
-        possible_rotations = [i for i, x in enumerate(sorted_nums) if x == nums[0]]
+        arr = nums.copy()
+        if nums.sort() == nums:
+            return True
+        else:
+            for i in range(len(arr)):
+                arr.append(arr.pop(0))
+                if arr == nums:
+                    return True
         
-        for num_rotation in possible_rotations:
-            rotations = (len(nums) - num_rotation) % len(nums)
-            
-            sorted_numsQ = deque(sorted_nums)
-            print("Nums =", nums)
-            print("Sorted Nums List =", sorted_nums)
-            print("Sorted Nums Queue =", sorted_numsQ, "type =", type(sorted_numsQ))
-            print("Testing Position =", num_rotation)
-            print("Rotations to perform =", rotations)
-            
-            # perform the right rotations on sorted_numsQ
-            for i in range(rotations):
-                sorted_numsQ.appendleft(sorted_numsQ.pop())
-            
-            print("Rotated Queue =", sorted_numsQ)
-            print("Rotated List =", list(sorted_numsQ))
-            
-            # compare the rotated list with the original nums and return True/False
-            if list(sorted_numsQ) == nums:
-                return True
         return False
+
+
+
+'''
+if the nums == nums.sort(): return TRUE
+else:
+     in a loop reverse rotate the nums by 1:
+         check at each rotation if rotated_nums == nums.sort():
+             return TRUE
+
+     return FALSE
+'''
